bf3a6a1ec9c910cf5683c5f8753492dd15fbcc6f
Jan Declercq <jan.declercq@alsic.be>
2021-12-03 13:13:06 +0100

    Added Code converter by parameter

--- DateTimeFormatTest.cs
+++ DateTimeFormatTest.cs
@@ -11,6 +11,11 @@ namespace example.linq2db.sqlite.datetimeformat {
 
         [ClassInitialize]
         public static void ClassInitialize(TestContext context) {
+            DataConnection.TurnTraceSwitchOn();
+            DataConnection.WriteTraceLine = (s1, s2, t) => {
+                Console.WriteLine($"Level {t}, Action: {s1}, Source: {s2}");
+            };
+
             //always use a new clean database in the TestDir..
             DataConnection.DefaultSettings = new MockDBSettings(context.TestDir);
             MyDB.Init();
@@ -22,12 +27,28 @@ namespace example.linq2db.sqlite.datetimeformat {
 
             using MyDB db = new();
 
-            db.Insert(new TestTable() { Id = 1, HistStartTime = d });
+            _ = await db.InsertAsync(new TestTable() { Id = 1, HistStartTime = d });
             TestTable? result = await db.TestTables.FirstOrDefaultAsync();
 
             Assert.AreEqual(d, result?.HistStartTime);
 
             MyClass? resultString = db.Query<MyClass>("select HistStartTime from TestTable").FirstOrDefault();
+            Assert.AreEqual(d.ToString("o"), resultString?.HistStartTime);
+        }
+
+
+        [TestMethod]
+        public async Task CheckDateTimeFormat2() {
+            DateTimeOffset d = new(2021, 1, 1, 6, 6, 7, TimeSpan.Zero);
+
+            using MyDB db = new();
+
+            db.Insert(new TestTable2() { Id = 1, HistStartTime = d });
+            TestTable2? result = await db.TestTables2.FirstOrDefaultAsync();
+
+            Assert.AreEqual(d, result?.HistStartTime);
+
+            MyClass? resultString = db.Query<MyClass>("select HistStartTime from TestTable2").FirstOrDefault();
             Assert.AreEqual(d.ToString("o"), resultString?.HistStartTime);
         }
 
--- MyDB.cs
+++ MyDB.cs
@@ -1,53 +1,84 @@
 ﻿using LinqToDB;
+using LinqToDB.Common;
 using LinqToDB.Data;
 using LinqToDB.Mapping;
 using System;
 using System.Globalization;
 using System.Linq;
 
-namespace example.linq2db.sqlite.datetimeformat {
-    public class MyDB : DataConnection {
-        private static readonly IFormatProvider _Provider = CultureInfo.InvariantCulture.DateTimeFormat;
-        private static readonly Lazy<MappingSchema> _mappingSchema = new(CreateMapping);
+namespace example.linq2db.sqlite.datetimeformat;
+public class MyDB : DataConnection {
+    private static readonly IFormatProvider _Provider = CultureInfo.InvariantCulture.DateTimeFormat;
+    private static readonly Lazy<MappingSchema> _mappingSchema = new(CreateMapping);
 
-        internal static class TableNames {
-            public const string TestTable = "TestTable";
-        }
-
-        public MyDB() : base("MyDB", _mappingSchema.Value) { }
-
-
-        public static void Init() {
-            CreateTables();
-        }
-
-        public static MappingSchema CreateMapping() {
-            MappingSchema? ret = new();
-            FluentMappingBuilder? m = ret.GetFluentMappingBuilder();
-            m.Entity<TestTable>()
-                .Property(x => x.HistStartTime)
-                .HasConversion(x => x.HasValue ? x.Value.ToString("O", _Provider) : string.Empty, x => string.IsNullOrWhiteSpace(x) ? null : DateTimeOffset.ParseExact(x, "O", _Provider));
-            return ret;
-        }
-
-        public static void CreateTables() {
-            using MyDB? db = new();
-
-            //Fetching the schema
-            LinqToDB.SchemaProvider.ISchemaProvider? sp = db.DataProvider.GetSchemaProvider();
-            LinqToDB.SchemaProvider.DatabaseSchema? dbSchema = sp.GetSchema(db);
-
-            if (!dbSchema.Tables.Any(t => t.TableName == MyDB.TableNames.TestTable)) { db.CreateTable<TestTable>(); }
-        }
-
-        public ITable<TestTable> TestTables => GetTable<TestTable>();
+    internal static class TableNames {
+        public const string TestTable = "TestTable";
+        public const string TestTable2 = "TestTable2";
     }
 
-    [Table("TestTable")]
-    public partial class TestTable {
+    public MyDB() : base("MyDB", _mappingSchema.Value) { }
 
-        [Column("ID"), PrimaryKey, Identity] public int Id { get; set; }
 
-        [Column("HistStartTime"), Nullable] public DateTimeOffset? HistStartTime { get; set; }
+    public static void Init() {
+        CreateTables();
+    }
+
+    public static MappingSchema CreateMapping() {
+        MappingSchema? ret = new();
+        FluentMappingBuilder? m = ret.GetFluentMappingBuilder();
+        m.Entity<TestTable>()
+            .Property(x => x.HistStartTime)
+            .HasConversion(
+                x => x.HasValue ? x.Value.ToString("O", _Provider) : string.Empty,
+                x => string.IsNullOrWhiteSpace(x) ? null : DateTimeOffset.ParseExact(x, "O", _Provider));
+        return ret;
+    }
+
+    public static void CreateTables() {
+        using MyDB? db = new();
+
+        //Fetching the schema
+        LinqToDB.SchemaProvider.ISchemaProvider? sp = db.DataProvider.GetSchemaProvider();
+        LinqToDB.SchemaProvider.DatabaseSchema? dbSchema = sp.GetSchema(db);
+
+        if (!dbSchema.Tables.Any(t => t.TableName == MyDB.TableNames.TestTable)) { db.CreateTable<TestTable>(); }
+        if (!dbSchema.Tables.Any(t => t.TableName == MyDB.TableNames.TestTable2)) { db.CreateTable<TestTable2>(); }
+    }
+
+    public ITable<TestTable> TestTables => GetTable<TestTable>();
+    public ITable<TestTable2> TestTables2 => GetTable<TestTable2>();
+}
+
+public class DateTimeOffsetConverterAttribute : ValueConverterAttribute {
+    public DateTimeOffsetConverterAttribute() : base() {
+        ValueConverter = new DateTimeOffsetConverter();
+        ConverterType = typeof(DateTimeOffsetConverter);
     }
 }
+
+internal class DateTimeOffsetConverter : ValueConverter<DateTimeOffset?, string?> {
+    private static readonly IFormatProvider _Provider = CultureInfo.InvariantCulture.DateTimeFormat;
+    public DateTimeOffsetConverter() : base(
+        input => input.HasValue ? input.Value.ToString("O", _Provider) : string.Empty,
+        input => string.IsNullOrWhiteSpace(input) ? null : DateTimeOffset.ParseExact(input, "O", _Provider),
+        false) {
+    }
+}
+
+[Table(MyDB.TableNames.TestTable)]
+public partial class TestTable {
+
+    [Column("ID"), PrimaryKey, Identity] public int Id { get; set; }
+
+    [Column("HistStartTime"), Nullable] public DateTimeOffset? HistStartTime { get; set; }
+}
+
+
+[Table(MyDB.TableNames.TestTable2)]
+public partial class TestTable2 {
+
+    [Column("ID"), PrimaryKey, Identity] public int Id { get; set; }
+
+    [Column("HistStartTime"), Nullable, DateTimeOffsetConverter] public DateTimeOffset? HistStartTime { get; set; }
+}
+
